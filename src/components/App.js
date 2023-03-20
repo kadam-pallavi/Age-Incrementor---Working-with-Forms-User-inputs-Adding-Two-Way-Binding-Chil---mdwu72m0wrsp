77770b45fa076bf6d496f815b606cac5aaafe0a7
kadam-pallavi <117823209+kadam-pallavi@users.noreply.github.com>
2023-03-20 15:11:40 +0530

    Initial commit

---
+++ src/components/App.js
@@ -0,0 +1,10 @@
+import React, { useState } from "react";
+import "../styles/App.css";
+import Child from "./Child";
+const App = () => {
+    function incrementAge() {}
+
+    return <Child />;
+};
+
+export default App;
---
+++ src/components/Child.js
@@ -0,0 +1,7 @@
+import React from "react";
+
+function Child() {
+    return <div id="child"></div>;
+}
+
+export default Child;
